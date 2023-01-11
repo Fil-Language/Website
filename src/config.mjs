c83bb66c44124975ec872115d3316205087470f6
Kevin Traini <kevin@ktraini.com>
2023-01-11 17:39:16 +0100

    Remove all useless files

--- src/config.mjs
+++ src/config.mjs
@@ -15,47 +15,6 @@ const CONFIG = {
 
   language: 'en',
   textDirection: 'ltr',
-
-  dateFormatter: new Intl.DateTimeFormat('en', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    timeZone: 'UTC',
-  }),
-
-  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-  googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
-
-  blog: {
-    disabled: false,
-    postsPerPage: 4,
-
-    list: {
-      pathname: 'doc', // doc main path
-      noindex: false,
-      disabled: false,
-    },
-
-    post: {
-      pathname: 'doc', // value for /doc/some-post
-      noindex: false,
-      disabled: false,
-    },
-
-    category: {
-      pathname: 'category', // set empty to change from /category/some-category to /some-category
-      noindex: true,
-      disabled: false,
-    },
-
-    tag: {
-      pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
-      noindex: true,
-      disabled: false,
-    },
-  },
 };
 
-export const SITE = { ...CONFIG, blog: undefined };
-export const BLOG = CONFIG.blog;
-export const DATE_FORMATTER = CONFIG.dateFormatter;
+export const SITE = CONFIG;
