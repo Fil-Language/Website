aacf661a3834d9a9db5a49607627e7ab811db3f5
Kevin Traini <kevin@ktraini.com>
2023-01-11 17:51:05 +0100

    Finish delete useless files

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,9 +1,8 @@
-import slugify from 'limax';
-
-import { SITE, BLOG } from '~/config.mjs';
+import { SITE } from '~/config.mjs';
 import { trim } from '~/utils/utils';
 
 const trimSlash = (s: string) => trim(trim(s, '/'));
+
 const createPath = (...params: string[]) => {
   const paths = params
     .map((el) => trimSlash(el))
@@ -14,51 +13,13 @@ const createPath = (...params: string[]) => {
 
 const BASE_PATHNAME = SITE.basePathname;
 
-export const cleanSlug = (text = '') =>
-  trimSlash(text)
-    .split('/')
-    .map((slug) => slugify(slug))
-    .join('/');
-
-export const BLOG_BASE = cleanSlug(BLOG?.list?.pathname);
-export const POST_BASE = cleanSlug(BLOG?.post?.pathname);
-export const CATEGORY_BASE = cleanSlug(BLOG?.category?.pathname);
-export const TAG_BASE = cleanSlug(BLOG?.tag?.pathname);
-
 /** */
 export const getCanonical = (path = ''): string | URL => new URL(path, SITE.origin);
 
 /** */
-export const getPermalink = (slug = '', type = 'page'): string => {
-  let permalink: string;
-
-  switch (type) {
-    case 'category':
-      permalink = createPath(CATEGORY_BASE, cleanSlug(slug));
-      break;
-
-    case 'tag':
-      permalink = createPath(TAG_BASE, cleanSlug(slug));
-      break;
-
-    case 'post':
-      permalink = createPath(POST_BASE, cleanSlug(slug));
-      break;
-
-    case 'page':
-    default:
-      permalink = createPath(slug);
-      break;
-  }
-
-  return definitivePermalink(permalink);
+export const getPermalink = (slug = ''): string => {
+  return createPath(BASE_PATHNAME, slug);
 };
 
 /** */
 export const getHomePermalink = (): string => getPermalink('/');
-
-/** */
-export const getBlogPermalink = (): string => getPermalink(BLOG_BASE);
-
-/** */
-const definitivePermalink = (permalink: string): string => createPath(BASE_PATHNAME, permalink);
